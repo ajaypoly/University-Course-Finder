27f140c293db3fbf6c6e682bcbb0273533a8b2f8
Ajay poly <ajaypoly99@gmail.com>
2023-10-27 10:59:32 +0530

    added pagination and list the courses

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import logo from "./assets/images/dom-fou-YRMWVcdyhmI-unsplash.jpg";
+import CourseLists from "./components/CourseList";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <img src={logo} alt="Logo" />
+      <CourseLists />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CourseList/index.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import "./style.css";
+import ReactPaginate from "react-paginate";
+import courseData from "../../data/Assignment_data.json";
+
+const itemsPerPage = 4;
+
+const CourseLists = () => {
+  const [data, setData] = useState(courseData); // Your data array
+  const [currentPage, setCurrentPage] = useState(0); // Current page for pagination
+  const pageCount = Math.ceil(data.length / itemsPerPage);
+
+  const getVisibleCourses = () => {
+    const startIndex = currentPage * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return data.slice(startIndex, endIndex);
+  };
+
+  const handlePageClick = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+  return (
+    <>
+      <div className="course-list-div">
+        {getVisibleCourses().map((course) => (
+          <div key={course.id} className="course-box">
+            <div className="course-heading-div">
+              <img
+                src={course.logo_path}
+                alt="Course Logo"
+                className="course-logo"
+              />
+              <h2 className="course-heading">{course.name}</h2>
+            </div>
+
+            <p className="course-description">{course.description}</p>
+
+            <div className="course-button-div">
+              {" "}
+              <button
+                className="glow-on-hover"
+                onClick={() => {
+                  window.open(course.url, "_blank");
+                }}
+              >
+                View More
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="course-pagination">
+        <ReactPaginate
+          className="course-pagination-component"
+          breakLabel="..."
+          nextLabel="next >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="< previous"
+          renderOnZeroPageCount={null}
+          forcePage={currentPage}
+        />
+      </div>
+    </>
+  );
+};
+
+export default CourseLists;
